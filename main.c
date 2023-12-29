5fc4cee1efd9a24a97afbfd7f24b85e2aa2226dc
NujanZh <NujanZh@gmail.com>
2023-12-29 14:57:26 +0100

    safely took the code from the check.c file and used it in main.c

--- main.c
+++ main.c
@@ -18,15 +18,20 @@ int check_too_many_parameters(char** argv, int i, int count_parameter);
 
 int main(int argc, char** argv) 
 {
+    char buffer[1000];
+    char *data;
+    int days = 0;
 
     FILE* real_data_file = NULL;
     FILE* output_file = NULL;
 
     int count_real_data_arg = 0;
     int count_output_arg = 0;
+    int count_infectious_days = 0;
+    int count_infections_per_day = 0;
 
     int infectious_days = 0;
-    int infectious_per_day = 0;
+    double infections_per_day = 0;
 
     for (int i = 1; i < argc; i++)
     {
@@ -47,8 +52,9 @@ int main(int argc, char** argv)
         } else if (strcmp(argv[i], "--infectious_days") == 0)
         {
             i++;
+            count_infectious_days++;
 
-            if (check_too_many_parameters(argv, i, infectious_days)) return 1;
+            if (check_too_many_parameters(argv, i, count_infectious_days)) return 1;
             if (i >= argc || argv[i][0] == '-')
             {
                 printf("Missing integer to file for %s\n", argv[i - 1]);
@@ -59,20 +65,21 @@ int main(int argc, char** argv)
 
             infectious_days = num;
 
-        } else if (strcmp(argv[i], "--infectious_per_day") == 0)
+        } else if (strcmp(argv[i], "--infections_per_day") == 0)
         {
             i++;
+            count_infections_per_day++;
 
-            if (check_too_many_parameters(argv, i, infectious_per_day)) return 1;
+            if (check_too_many_parameters(argv, i, count_infections_per_day)) return 1;
             if (i >= argc || argv[i][0] == '-')
             {
                 printf("Missing integer to file for %s\n", argv[i - 1]);
                 return 1;
             }
 
-            int num = atoi(argv[i]);
+            double num = atof(argv[i]);
 
-            infectious_per_day = num;
+            infections_per_day = num;
 
         } else if (strcmp(argv[i], "--output") == 0)
         {
@@ -91,14 +98,55 @@ int main(int argc, char** argv)
     }
 
     check_missing_parameter("--real-data", count_real_data_arg);
-    check_missing_parameter("--infectious_days", infectious_days);
-    check_missing_parameter("--infectious_per_day", infectious_per_day);
+    check_missing_parameter("--infectious_days", count_infectious_days);
+    check_missing_parameter("--infections_per_day", count_infections_per_day);
     check_missing_parameter("--output", count_output_arg);
 
-    double b = infectious_per_day;
+    double b = infections_per_day;
     double y = 1.0 / (double)infectious_days;
 
+
+    fgets(buffer, sizeof(buffer), real_data_file);
+
+    data = strtok(buffer, ",");
+    int susceptible_number_start = atoi(data);
+
+    data = strtok(NULL, ",");
+    int infected_number_start = atoi(data);
+
+    data = strtok(NULL, ",");
+    int recovered_number_start = atoi(data);
+
+    int total_number = susceptible_number_start + infected_number_start + recovered_number_start;
+
+    while(fgets(buffer, sizeof(buffer), real_data_file))
+    {
+        if (days == 0)
+        {
+            printf("%d %d %d\n", susceptible_number_start, infected_number_start, recovered_number_start);
+            days++;
+        }
+
+        data = strtok(buffer, ",");
+        int susceptible_number = atoi(data);
+
+        data = strtok(NULL, ",");
+        int infected_number = atoi(data);
+
+        data = strtok(NULL, ",");
+        int recovered_number = atoi(data);
+
+        printf("%d %d %d\n", susceptible_number, infected_number, recovered_number);
+
+        days++;
+    }
+
+    printf("%d\n", days);
+
     EpidemicState state;
+    state.S = (double) susceptible_number_start / total_number;
+    state.I = (double) infected_number_start / total_number;
+    state.R = (double) recovered_number_start / total_number;
 
     fclose(real_data_file);
     fclose(output_file);
